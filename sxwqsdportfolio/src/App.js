e5bfb63c764bb8495edd5fb30c681db34a9b5fde
squid <mercado.valeria856@gmail.com>
2021-11-01 21:21:06 -0700

    i redid it

--- sxwqsdportfolio/src/App.js
+++ sxwqsdportfolio/src/App.js
@@ -3,15 +3,7 @@ import './App.css';
 import Nav from './components/Nav';
 import About from './components/About'
 
-const categories = [
-  {
-    name: "Works",
-    description:
-      "Links to projects I've led or been apart of.",
-  },
-  { name: "Resume", description: "For a more in dept tellign of my expierience" },
- 
-];
+
 
 function App() {
   return (
