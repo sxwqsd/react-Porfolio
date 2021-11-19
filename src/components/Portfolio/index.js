f10e4f3c353518b6c9db07309cf13aed9f364325
squid <mercado.valeria856@gmail.com>
2021-11-18 21:38:58 -0700

    added Otter Journal

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -3,6 +3,7 @@ import foodjunkies from '../../assets/img/foodJunkies.png'
 import sloothhound from '../../assets/img/sloothHound.png'
 import notetaker from '../../assets/img/noteTaker.png'
 import workday from '../../assets/img/workdayScheduler.png'
+import OtterJournal from '../../assets/img/OtterJournal.png'
 
 
 function Portfolio(){
@@ -26,6 +27,10 @@ function Portfolio(){
             </a>
             <a class="my-5" href="https://sxwqsd.github.io/Work-Day-Scheduler/">
                 <img src= {workday}alt="Work-Day-Scheduler"
+                    width="309px" height="auto"></img>
+            </a>
+            <a class="my-5" href="https://quiet-waters-13147.herokuapp.com/">
+                <img src= {OtterJournal}alt="OtterJournal"
                     width="309px" height="auto"></img>
             </a>
         
